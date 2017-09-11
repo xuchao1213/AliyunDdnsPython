1cdba2752116080d6a163754f4589040602ebb04
Xuchao <xuchao_1213@163.com>
2017-09-11 23:59:20 +0800

    完善ddns.py

--- ddns.py
+++ ddns.py
@@ -12,17 +12,30 @@ def main():
     Main Function
     """
     print "ali-ddns run ..."
-    print ("agr len :" + str(len(sys.argv)))
-    print(str(sys.argv[0]))
-
     local_ip = Util.getLocalInternetIp()
     if local_ip[0] is False:
         print local_ip[1]
         return
     """
-    //TODO 更新IP
+    load config ,init AliYunSdkWrapper
     """
-
+    print "local ip " + local_ip
+    sdkUtil = AliYunSdkWrapper(
+        "access_key_id",
+        "access_key_secret",
+        "domain_name",
+        "sub_domain_name",
+        "A",
+        "cn-hangzhou"
+    )
+    """
+   update the record
+    """
+    res = sdkUtil.getRecordValue()
+    if res[0]:
+        sdkUtil.setRecordValue(local_ip[1])
+    else:
+        print res[1] + "[Skip]"
 
 if __name__ == "__main__":
     main()
