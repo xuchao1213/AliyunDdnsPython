9d9db9234f6627504a1db746f64c4bc0d88b4c5b
Xuchao <xuchao_1213@163.com>
2017-09-13 00:05:47 +0800

    finish the config

--- config.py
+++ config.py
@@ -1,2 +1,47 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import sys
+
+if sys.version_info < (3,):
+    import ConfigParser
+else:
+    import configparser as ConfigParser
+
+
+class Config:
+    def __init__(self):
+        self.interval = 30 * 60 * 1000
+        self.access_key_id = None
+        self.access_key_secret = None
+        self.domain_name = None
+        self.sub_domain_name = None
+        self.record_type = "A"
+        self.region_id = "cn-hangzhou"
+
+        self.configParser = ConfigParser.ConfigParser()
+
+    def loadConfig(self, file):
+        if not self.configParser.read(file):
+            print "config file not exist"
+            return False
+        try:
+            self.access_key_id = self.configParser.get("CONFIG", "access_key_id")
+            self.access_key_secret = self.configParser.get("CONFIG", "access_key_secret")
+            self.domain_name = self.configParser.get("CONFIG", "domain_name")
+            self.sub_domain_name = self.configParser.get("CONFIG", "sub_domain_name")
+            self.record_type = self.configParser.get("CONFIG", "record_type")
+            self.region_id = self.configParser.get("CONFIG", "region_id")
+            if not self.record_type:
+                self.record_type = "A"
+            if not self.region_id:
+                self.region_id = "cn-hangzhou"
+        except Exception, e:
+            print "invalid config: {0}".format(e.message)
+            return False
+
+        if not self.access_key_id or not self.access_key_secret or not self.domain_name or not self.sub_domain_name:
+            print "invalid config"
+            return False
+
+        return True
--- ddns.py
+++ ddns.py
@@ -5,6 +5,9 @@ import sys
 
 from sdkwrapper import AliYunSdkWrapper
 from util import Util
+from config import Config
+
+CONFIG_FILE = "ddns.conf"
 
 
 def main():
@@ -12,30 +15,39 @@ def main():
     Main Function
     """
     print "ali-ddns run ..."
-    local_ip = Util.getLocalInternetIp()
-    if local_ip[0] is False:
-        print local_ip[1]
-        return
     """
-    load config ,init AliYunSdkWrapper
+    load config 
     """
-    print "local ip " + local_ip
-    sdkUtil = AliYunSdkWrapper(
-        "access_key_id",
-        "access_key_secret",
-        "domain_name",
-        "sub_domain_name",
-        "A",
-        "cn-hangzhou"
-    )
-    """
-   update the record
-    """
-    res = sdkUtil.getRecordValue()
-    if res[0]:
-        sdkUtil.setRecordValue(local_ip[1])
-    else:
-        print res[1] + "[Skip]"
+    ddnsConfig = Config()
+    loadRes = ddnsConfig.loadConfig(CONFIG_FILE)
+    if loadRes:
+        """
+        get current local internet ip
+        """
+        local_ip = Util.getLocalInternetIp()
+        if local_ip[0] is False:
+            print local_ip[1]
+            return
+        """
+        init AliYunSdkWrapper
+        """
+        sdkUtil = AliYunSdkWrapper(
+            ddnsConfig.access_key_id,
+            ddnsConfig.access_key_secret,
+            ddnsConfig.domain_name,
+            ddnsConfig.sub_domain_name,
+            ddnsConfig.record_type,
+            ddnsConfig.region_id
+        )
+        """
+        update record
+        """
+        res = sdkUtil.getRecordValue()
+        if res[0]:
+            sdkUtil.setRecordValue(local_ip[1])
+        else:
+            print res[1] + "[Skip]"
+
 
 if __name__ == "__main__":
     main()
