d3a2d4a4fb50c74b36bd8b2bd8f702cb28750007
Xuchao <xuchao_1213@163.com>
2017-09-13 00:16:55 +0800

    config fix the miss 'interval'

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ else:
 
 class Config:
     def __init__(self):
-        self.interval = 30 * 60 * 1000
+        self.interval = 30
         self.access_key_id = None
         self.access_key_secret = None
         self.domain_name = None
@@ -26,12 +26,15 @@ class Config:
             print "config file not exist"
             return False
         try:
+            self.interval = self.configParser.getint("CONFIG", "interval")
             self.access_key_id = self.configParser.get("CONFIG", "access_key_id")
             self.access_key_secret = self.configParser.get("CONFIG", "access_key_secret")
             self.domain_name = self.configParser.get("CONFIG", "domain_name")
             self.sub_domain_name = self.configParser.get("CONFIG", "sub_domain_name")
             self.record_type = self.configParser.get("CONFIG", "record_type")
             self.region_id = self.configParser.get("CONFIG", "region_id")
+            if not self.interval:
+                self.interval = 30
             if not self.record_type:
                 self.record_type = "A"
             if not self.region_id:
