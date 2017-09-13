0feff40589dd9589d9451afb2371e77081871c45
Xuchao <xuchao_1213@163.com>
2017-09-14 00:50:59 +0800

    update dep.sh

--- dep.sh
+++ dep.sh
@@ -3,17 +3,27 @@
 # install python
 apt-get update |apt-get install python python-pip
 
+#stop exit service
+systemctl stop ddns
+systemctl disable ddns
+
+# remove exist file
+rm -r /opt/AliyunDdnsPython
+rm /usr/lib/systemd/system/ddns.service
+
 #install dependence
+pip install --upgrade pip
 pip install requests
 pip install aliyun-python-sdk-core
 pip install aliyun-python-sdk-alidns
 
 # copy file to /opt/AliyunDdnsPython
-mkdir /opt/AliyunDdnsPython
+mkdir -p /opt/AliyunDdnsPython
 cp *.* /opt/AliyunDdnsPython/
 
 # install service and run
-cp ddns.service /usr/lib/systemd/system/
+mkdir -p /usr/lib/systemd/system
+cp ddns.service /usr/lib/systemd/system/ddns.service
 systemctl daemon-reload
 systemctl enable ddns
 systemctl start ddns
